58eb9661811d0257baba3cc1b06a9aa25d0e6cba
jazz mickle <jaymickle@gmail.com>
2017-12-22 19:34:59 +0100

    Added images to engine wad
    Using PIL and omgifol to convert pngs to doom graphic format

--- build_wad.py
+++ build_wad.py
@@ -2,12 +2,21 @@ import omg
 from os import listdir
 from os.path import isfile, join
 import sys
+from PIL import Image
 
 wadfilespath = "enginewad/"
 wad = omg.WAD()
 files = [f for f in listdir(wadfilespath) if isfile(join(wadfilespath, f))]
 for f in files:
-	with open(wadfilespath+f, "rb") as file:
-		wad.data[f[:f.find('.')]] = omg.Lump(file.read())
+	ext = f[f.find('.')+1:]
+	if ext == "png":
+		graphic = omg.Graphic()
+		graphic.from_Image(Image.open(wadfilespath+f))
+		lump = graphic
+	else:
+		with open(wadfilespath+f, "rb") as file:
+			lump = omg.Lump(file.read())
+			
+	wad.data[f[:f.find('.')]] = lump
 
 wad.to_file("../../../nasty.wad")
