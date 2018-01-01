0ca3cbf5a463ee5219b03f5bad023fad4c17c995
jazz mickle <jaymickle@gmail.com>
2018-01-01 22:21:08 +0100

    Remove mouse interpolation

--- External/First Person Drifter Controller/Scripts/MouseLook.cs
+++ External/First Person Drifter Controller/Scripts/MouseLook.cs
@@ -20,22 +20,11 @@ public class MouseLook : MonoBehaviour
 	public float sensitivityX = 10F;
 	public float sensitivityY = 9F;
  
-	public float minimumX = -360F;
-	public float maximumX = 360F;
- 
 	public float minimumY = -85F;
 	public float maximumY = 85F;
  
 	float rotationX = 0F;
 	float rotationY = 0F;
- 
-	private List<float> rotArrayX = new List<float>();
-	float rotAverageX = 0F;	
- 
-	private List<float> rotArrayY = new List<float>();
-	float rotAverageY = 0F;
- 
-	public float framesOfSmoothing = 5;
  
 	Quaternion originalRotation;
 	
@@ -53,52 +42,23 @@ public class MouseLook : MonoBehaviour
 	{
 		if (axes == RotationAxes.MouseX)
 		{			
-			rotAverageX = 0f;
- 
 			rotationX += Input.GetAxis("Mouse X") * sensitivityX * Time.timeScale;
- 
-			rotArrayX.Add(rotationX);
- 
-			if (rotArrayX.Count >= framesOfSmoothing)
-			{
-				rotArrayX.RemoveAt(0);
-			}
-			for(int i = 0; i < rotArrayX.Count; i++)
-			{
-				rotAverageX += rotArrayX[i];
-			}
-			rotAverageX /= rotArrayX.Count;
-			rotAverageX = ClampAngle(rotAverageX, minimumX, maximumX);
- 
-			Quaternion xQuaternion = Quaternion.AngleAxis (rotAverageX, Vector3.up);
+			Quaternion xQuaternion = Quaternion.AngleAxis (rotationX, Vector3.up);
 			transform.localRotation = originalRotation * xQuaternion;			
 		}
 		else
 		{			
-			rotAverageY = 0f;
- 
- 			float invertFlag = 1f;
+			float invertFlag = 1f;
  			if( invertY )
  			{
  				invertFlag = -1f;
  			}
+
 			rotationY += Input.GetAxis("Mouse Y") * sensitivityY * invertFlag * Time.timeScale;
 			
 			rotationY = Mathf.Clamp(rotationY, minimumY, maximumY);
- 	
-			rotArrayY.Add(rotationY);
  
-			if (rotArrayY.Count >= framesOfSmoothing)
-			{
-				rotArrayY.RemoveAt(0);
-			}
-			for(int j = 0; j < rotArrayY.Count; j++)
-			{
-				rotAverageY += rotArrayY[j];
-			}
-			rotAverageY /= rotArrayY.Count;
- 
-			Quaternion yQuaternion = Quaternion.AngleAxis (rotAverageY, Vector3.left);
+			Quaternion yQuaternion = Quaternion.AngleAxis (rotationY, Vector3.left);
 			transform.localRotation = originalRotation * yQuaternion;
 		}
 	}
@@ -107,19 +67,5 @@ public class MouseLook : MonoBehaviour
 	{
 		sensitivityX = s;
 		sensitivityY = s;
-	}
- 
-	public static float ClampAngle (float angle, float min, float max)
-	{
-		angle = angle % 360;
-		if ((angle >= -360F) && (angle <= 360F)) {
-			if (angle < -360F) {
-				angle += 360F;
-			}
-			if (angle > 360F) {
-				angle -= 360F;
-			}			
-		}
-		return Mathf.Clamp (angle, min, max);
 	}
 }
