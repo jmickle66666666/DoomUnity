84bf0b85f2b492256cd2e160176b4a6fd77f3143
jazz mickle <jaymickle@gmail.com>
2018-01-01 19:38:59 +0100

    Add textured automap

--- GameScripts/CameraDepth.cs
+++ GameScripts/CameraDepth.cs
@@ -9,16 +9,33 @@ This needs to be attached to the player camera, and is necessary for correct sky
 public class CameraDepth : MonoBehaviour {
 
 	public Material mat;
+	private bool automap = false;
+	private Camera cam;
 
 	// Use this for initialization
 	void Start () {
-		Camera camera = GetComponent<Camera>();
-		camera.depthTextureMode = DepthTextureMode.Depth;
+		cam = GetComponent<Camera>();
+		cam.depthTextureMode = DepthTextureMode.Depth;
 	}
 
 	void Update() {
 		if (DoomMapBuilder.skyMaterial != null) {
 			DoomMapBuilder.skyMaterial.SetFloat("_CameraAngle", transform.eulerAngles.y);
+		}
+
+		if (Input.GetKeyDown(KeyCode.Tab)) {
+			if (automap) {
+				transform.localEulerAngles = new Vector3(0f, 0f, 0f);
+				transform.localPosition = new Vector3(0f, 0.5f, 0f);
+				cam.orthographic = false;
+				automap = false;
+			} else {
+				transform.localEulerAngles = new Vector3(90f, 0f, 0f);
+				transform.localPosition = new Vector3(0f, 100f, 0f);
+				cam.orthographic = true;
+				cam.orthographicSize = 15;
+				automap = true;
+			}
 		}
 	}
 }
