f7c8e2e5fc7089b14b55f8f365c8c0803a126050
jazz mickle <jaymickle@gmail.com>
2018-05-14 16:16:15 +0200

    Prelim

---
+++ GameScripts/LevelComponents/MapSector.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapSector : MonoBehaviour {
+
+	/* WHATS HAPPENING HERE
+	 let me tell you
+
+	The map will be built out of game objects representing sectors
+	these will contain 
+
+	*/
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
